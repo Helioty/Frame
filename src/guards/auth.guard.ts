3eef5d05b262ed8b65d05557bdc836963fc19389
Hélio Filho <contatohelioseverino@gmail.com>
2019-12-01 18:15:24 -0300

    "AuthGuard"
    Criado o primeiro "guard" do aplicativo utilidado no login.

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  public logged: boolean = false;
+
+  constructor(private router: Router) {
+
+  }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+
+    return this.logged
+
+  }
+}
