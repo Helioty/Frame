e98ec94e5b62bf40651f8d37e7929c91d2136c52
Hélio Filho <contatohelioseverino@gmail.com>
2019-12-01 18:16:34 -0300

    AuthGuard
    Adicionado a guarda na tela 'home'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from './../guards/auth.guard';
+
 const routes: Routes = [
   {
     path: '',
@@ -9,7 +11,8 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'list',
