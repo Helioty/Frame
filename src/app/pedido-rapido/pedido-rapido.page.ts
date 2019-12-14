3a94cffad099d351ef77c9ab9dca8c15390c3e33
Hélio Filho <contatohelioseverino@gmail.com>
2019-12-14 18:38:11 -0300

    Alterações visuais

--- src/app/pedido-rapido/pedido-rapido.page.ts
+++ src/app/pedido-rapido/pedido-rapido.page.ts
@@ -9,29 +9,37 @@ export class PedidoRapidoPage implements OnInit {
 
   public itens = [{
     nome: 'TV',
-    qtd: 2
+    qtd: 2,
+    preco: 1999
   }, {
     nome: 'Geladeira',
-    qtd: 1
+    qtd: 1,
+    preco: 1499
   }, {
     nome: 'Furadeira',
-    qtd: 1
+    qtd: 1,
+    preco: 199
   }, {
     nome: 'Cama',
-    qtd: 2
+    qtd: 2,
+    preco: 259
   }, {
     nome: 'Ventilador',
-    qtd: 3
+    qtd: 3,
+    preco: 119
   }];
 
-  constructor() {
-    const el = document.querySelector('.item-inner');
-    console.log(el)
-    // el.style.setProperty('--background', '#36454f');
-  }
+  constructor() { }
 
   ngOnInit() {
     console.log(this.itens)
   }
 
+  add(id: any) {
+    this.itens[id].qtd++
+  }
+
+  rm(id: any) {
+    this.itens[id].qtd--
+  }
 }
