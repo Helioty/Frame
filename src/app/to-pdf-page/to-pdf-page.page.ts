d17fb9430df9d38cf557edf6c1021f05502c6c9a
Helio Filho <helio.souza@fc.com.br>
2020-01-23 14:14:01 -0300

    To pdf

--- src/app/to-pdf-page/to-pdf-page.page.ts
+++ src/app/to-pdf-page/to-pdf-page.page.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 import { HTMLToPDFAPIService } from '../../commons/services/html-to-pdf-api.service';
+
+import { BaseCommon } from 'src/commons/base-common';
 
 @Component({
   selector: 'app-to-pdf-page',
@@ -9,10 +12,138 @@ import { HTMLToPDFAPIService } from '../../commons/services/html-to-pdf-api.serv
 })
 export class ToPDFPagePage implements OnInit {
 
-  constructor(public toPDF: HTMLToPDFAPIService) { }
+  public siteUrl: string = "";
+  public formato: string = "";
+  public pageOrientation: string = "";
+  
+  public opcsFormato = ['a0', 'a1', 'a2', 'a3', 'a4', 'a5']
+  public opcsOrientation = ['auto', 'landscape', 'portrait']
+
+  
+
+  
+  public customActionSheetOptions: any = {
+    header: 'Formato de impressão',
+    // subHeader: 'Select your favorite color'
+  };
+
+  constructor(
+    public alertController: AlertController,
+    public toPDF: HTMLToPDFAPIService,
+    public common: BaseCommon, 
+    ) { }
 
   ngOnInit() {
-    this.toPDF.api()
+    // this.common.showLoader()
+    // let result = this.toPDF.api()
+    // console.log(result)
+  }
+
+  async clickToGenerate() {
+    try {
+      this.options()
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async options() {
+    let json: any = {
+      uri: 'https://api.sejda.com/v2/html-pdf',
+      headers: {
+        'Authorization': 'Token: ' + 'api_9472C6123CEC4DD2BF4123D75FF9A9AF',
+      },
+      json: {
+        'url': this.siteUrl,
+        'viewportWidth': 2560,
+        'pageSize': this.formato,
+        'pageOrientation': this.pageOrientation
+      }
+    }
+    await this.toPDF.api(json)
+  }
+
+
+  async presentAlertRadio() {
+    const alert = await this.alertController.create({
+      header: 'Radio',
+      inputs: [
+        {
+          name: 'radio1',
+          type: 'radio',
+          label: 'Radio 1',
+          value: 'value1',
+          checked: true
+        },
+        {
+          name: 'radio2',
+          type: 'radio',
+          label: 'Radio 2',
+          value: 'value2'
+        },
+        {
+          name: 'radio3',
+          type: 'radio',
+          label: 'Radio 3',
+          value: 'value3'
+        },
+        {
+          name: 'radio4',
+          type: 'radio',
+          label: 'Radio 4',
+          value: 'value4'
+        },
+        {
+          name: 'radio5',
+          type: 'radio',
+          label: 'Radio 5',
+          value: 'value5'
+        },
+        {
+          name: 'radio6',
+          type: 'radio',
+          label: 'Radio 6 Radio 6 Radio 6 Radio 6 Radio 6 Radio 6 Radio 6 Radio 6 Radio 6 Radio 6 ',
+          value: 'value6'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: () => {
+            console.log('Confirm Ok');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+
+
+  // @ViewChild('screen', {static: false}) screen: ElementRef;
+  // @ViewChild('canvas', {static: false}) canvas: ElementRef;
+  // @ViewChild('downloadLink', {static: false}) downloadLink: ElementRef;
+
+  // downloadImage(){
+  //   html2canvas(this.screen.nativeElement).then(canvas => {
+  //     this.canvas.nativeElement.src = canvas.toDataURL();
+  //     this.downloadLink.nativeElement.href = canvas.toDataURL('image/png');
+  //     this.downloadLink.nativeElement.download = 'marble-diagram.png';
+  //     this.downloadLink.nativeElement.click();
+  //   });
+  // }
+
+  consol() {
+    console.log(this.toPDF.resultOfPDF)
+    console.log(this.formato)
   }
 
 }
--- src/commons/services/html-to-pdf-api.service.ts
+++ src/commons/services/html-to-pdf-api.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { BaseCommon } from '../base-common';
 
-// import { require } from 'request';
+import {
+  HttpEvent,
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest,
+} from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { promise } from 'protractor';
+
+// import { request } from 'require';
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,42 +22,90 @@ export class HTMLToPDFAPIService {
 
   private ApiKey = "api_9472C6123CEC4DD2BF4123D75FF9A9AF";
 
+  public resultOfPDF: any;
+
   constructor(
     public http: HttpClient,
-    public common: BaseCommon
+    public common: BaseCommon,
   ) {
-    
+
   }
 
-  api() {
-    // const request = require('request');
-    // const fs = require('fs');
+  async api(json: any) {
+    this.common.showLoader()
+    let url = 'http://10.131.9.81:8080/pdf';
 
-    // let opts = {
-    //   uri: 'https://api.sejda.com/v2/html-pdf',
-    //   headers: {
-    //     'Authorization': 'Token: ' + this.ApiKey,
-    //   },
-    //   json: {
-    //     'url': 'https://example.com',
-    //     'viewportWidth': 1200
-    //   }
-    // };
+    const headers = new HttpHeaders().set('Content-type', 'application/json')
 
-    // request.post(opts)
-    //   .on('error', function (err) {
-    //     return console.error(err);
+    this.http.post<JSON>(url, json, { headers: headers }).subscribe(result => {
+      console.log(result);
+      this.resultOfPDF = result;
+      this.common.loading.dismiss();
+      if (this.resultOfPDF.status == 200) {
+        this.common.showAlert(this.resultOfPDF.title, this.resultOfPDF.text)
+      }
+    }, (error) => {
+      console.log(error)
+      this.common.loading.dismiss();
+      alert(error.message)
+    });
+    // let result: any = await this.callServer(url, json)
+    // return result
+  }
+
+  async callServer(url: string, json: any) {
+    const headers = new HttpHeaders().set('Content-type', 'application/json')
+
+    this.http.post<JSON>(url, json, { headers: headers }).subscribe(result => {
+      console.log(result);
+      let s: any = result;
+      this.common.loading.dismiss();
+      if (s.status == 200) {
+        this.common.showAlert(s.title, s.text)
+      }
+      return result
+    }, (error) => {
+      console.log(error)
+      this.common.loading.dismiss();
+      alert(error.message)
+    });
+  }
+
+}
+
+
+
+
+
+
+// this.ionhttp.get('http://ionic.io', {}, {})
+    //   .then(data => {
+    //     console.log(data)
+    //     // console.log(data.status);
+    //     // console.log(data.data); // data received by server
+    //     // console.log(data.headers);
+
     //   })
-    //   .on('response', function (response) {
-    //     if (response.statusCode === 200) {
-    //       response.pipe(fs.createWriteStream('/tmp/out.pdf'))
-    //         .on('finish', function () {
-    //           console.log('PDF saved to disk');
-    //         });
-    //     } else {
-    //       return console.error('Got code: ' + response.statusCode);
-    //     }
+    //   .catch(error => {
+
+    //     console.log(error)
+    //     // console.log(error.status);
+    //     // console.log(error.error); // error message as string
+    //     // console.log(error.headers);
+
     //   });
 
-  }
-}
+    // var http = new XMLHttpRequest();
+    // // var url = 'get_data.php';
+    // // var params = 'orem=ipsum&name=binny';
+    // http.open('POST', url, true);
+
+    // //Send the proper header information along with the request
+    // http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    // http.onreadystatechange = function () {//Call a function when the state changes.
+    //   if (http.readyState == 4 && http.status == 200) {
+    //     alert(http.responseText);
+    //   }
+    // }
+    // http.send();
