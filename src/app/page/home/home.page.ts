8a73712718b9dbfaf8642d43afa1483814ea4583
helio.souza <39842088+Helioty@users.noreply.github.com>
2020-08-13 13:42:51 -0300

    add scanner exemple

--- src/app/page/home/home.page.ts
+++ src/app/page/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CommonService } from 'src/app/services/common/common.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  @ViewChild('scanner', { static: true }) scanner: HTMLElement;
+  public valorScanner: string;
+  private focusStatus = true;
+  private taskScanner: any;
 
-  ngOnInit() {
+  constructor(private common: CommonService) { }
+
+  ngOnInit() { }
+
+  ionViewWillEnter() {
+    this.focusOn();
+    this.common.goToFullScreen();
+  }
+
+  ionViewDidEnter() {
+    this.common.goToFullScreen();
+  }
+
+  ionViewWillLeave() {
+    this.focusOff();
+  }
+
+  ionViewDidLeave() { }
+
+  // Cria o loop que da foco no input
+  focusOn() {
+    this.taskScanner = setInterval(() => {
+      try {
+        this.valorScanner = '';
+        if (this.focusStatus) {
+          const scanners = document.body.getElementsByClassName('scanner');
+          for (const i in scanners) {
+            if (Number(i) === (scanners.length - 1)) {
+              (<HTMLInputElement>scanners[i]).focus();
+            }
+          }
+          this.scanner.focus();
+        }
+      } catch (error) { }
+    }, 400);
+  }
+
+  focusPlay() {
+    this.focusStatus = true;
+  }
+
+  focusPause() {
+    this.focusStatus = false;
+    document.getElementById('scanner').blur();
+  }
+
+  // Encerra o loop de foco no input
+  focusOff() {
+    clearInterval(this.taskScanner);
   }
 
 }
