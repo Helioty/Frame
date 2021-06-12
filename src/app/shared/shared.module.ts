32a4757e8211e2787d5828e324cf3f6895205070
Hélio Filho <39842088+Helioty@users.noreply.github.com>
2021-06-12 20:21:31 -0300

    c

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HideKeyboardDirective } from './custom/hide-keyboard.directive';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    HideKeyboardDirective
+    HideKeyboardDirective,
+    MenuComponent
   ],
   imports: [
     CommonModule
