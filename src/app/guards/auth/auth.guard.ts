a51f0e7d152ea94d13eeb9fa9cba5f53cac760ca
helio.souza <helio.souza@ferreiracosta.com.br>
2021-06-12 19:22:15 -0300

    Update auth.guard.ts

--- src/app/guards/auth/auth.guard.ts
+++ src/app/guards/auth/auth.guard.ts
@@ -1,25 +1,36 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate, Router } from '@angular/router';
-import { CommonService } from 'src/app/services/common/common.service';
+import { CanActivate, Router } from '@angular/router';
+import { CommonService } from 'src/app/shared/services/common/common.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class AuthGuard implements CanActivate {
+  private logged = false;
 
-  public logged = false;
+  constructor(private router: Router, private common: CommonService) {}
 
-  constructor(private router: Router, private common: CommonService) { }
-
-  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+  canActivate(): boolean {
     if (!this.logged) {
-      this.common.showAlertInfo('Login necessário!');
-      return this.router.navigateByUrl('/login');
+      this.common.showAlert('Atenção!', 'Login necessário!');
+      this.router.navigateByUrl('/login');
+      return false;
     } else {
       return true;
     }
-
   }
 
+  /**
+   * @description Retorna o estado do login.
+   */
+  get logStatus(): boolean {
+    return this.logged;
+  }
+
+  /**
+   * @description Seta o estado do login.
+   */
+  set setStatus(st: boolean) {
+    this.logged = st;
+  }
 }
